336685aac701b6e62cdf1eb3ba444044d4cd12bc
Orkhan <oabiyev54@gmail.com>
2024-04-27 00:52:35 +0400

    Adding repository for User entity

---
+++ src/main/java/repos/UserRepo.java
@@ -0,0 +1,8 @@
+package repos;
+
+import model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User,Long> {
+    User findByEmail(String email);
+}
